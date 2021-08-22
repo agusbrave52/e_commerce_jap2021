79c1c97dd4059f16ffe35a3606f9a6f6073f011b
agusbrave52 <urubravo52@gmail.com>
2021-08-22 00:39:45 -0300

    Entrega 1
    Todo listo la entrega 1

--- js/login.js
+++ js/login.js
@@ -1,6 +1,26 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
 
 });
+function loguear() { // Creo la funcion para poder loguear() para terminar de verificar y loguearse
+    let usuario = document.getElementById("usuario"); //traigo datos del input usuario
+    let contrasena = document.getElementById("contrasena"); //traigo datos del input contraseña
+    
+    if (usuario.checkValidity() && contrasena.checkValidity()) { //valido con la funcion checkValidity() que verifica las validaciones del HTML y devuelve un booleano
+        window.location = "/home.html" //redirijo la pagina a home
+    }
+    else {
+        if (usuario.checkValidity() == false) { //hago unos if para cambiar el mensaje de error del usuario y mas abajo contraseña
+            let error = document.getElementById("msContrasena");
+            error.innerHTML = usuario.validationMessage + ": Usuario";
+        }
+        else {
+            if (contrasena.checkValidity() == false) {
+                let error = document.getElementById("msContrasena");
+                error.innerHTML = contrasena.validationMessage + ": Contraseña";
+            }
+        }
+    }
+}
--- js/products.js
+++ js/products.js
@@ -1,6 +1,35 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+function showProductList(currentProductArray){//la funcion recibe una lista la cual le damos el valor del response.data
+    let htmlContentToAppend = "";
+    //creamos un for para mostrar la lista de productos
+    for(let i = 0; i < currentProductArray.length; i++){
+        let product = currentProductArray[i];
+            htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${product.imgSrc}" alt="${product.description}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${product.name}</h4>
+                            <small class="text-muted">${product.soldCount} artículos</small>
+                        </div>
+                        <p class="mb-1">${product.description}</p>
+                        <small>${product.cost} ${product.currency}</small>
+                    </div>
+                    
+                </div>
+            </a>
+            `; //Agrego el precio que le faltaba
+        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
+    }
+}
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(response => {//usamos el getJSONData para traer los datos de los productos
+        showProductList(response.data);
+    });
+    
 });
