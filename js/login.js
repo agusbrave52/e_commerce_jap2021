2e2066c923a47ca036be10df22fdb93d5475c4c0
agusbrave52 <urubravo52@gmail.com>
2021-08-22 19:44:11 -0300

    cambio en el login.js
    le borre la barra porq no redirigia bien

--- js/login.js
+++ js/login.js
@@ -9,7 +9,7 @@ function loguear() { // Creo la funcion para poder loguear() para terminar de ve
     let contrasena = document.getElementById("contrasena"); //traigo datos del input contraseña
     
     if (usuario.checkValidity() && contrasena.checkValidity()) { //valido con la funcion checkValidity() que verifica las validaciones del HTML y devuelve un booleano
-        window.location = "/home.html" //redirijo la pagina a home
+        window.location = "home.html" //redirijo la pagina a home
     }
     else {
         if (usuario.checkValidity() == false) { //hago unos if para cambiar el mensaje de error del usuario y mas abajo contraseña
